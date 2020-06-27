637f93df3ada80cd5e891a8eacd5a5c9d6ed86e7
Hugo Bottois <bottois.hugo@outlook.fr>
2020-06-27 17:41:24 +0200

    routes simplify

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,21 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :contracts, only: [ :new, :create, :show, :index] do
-    resources :programs, only: [ :new, :create, :update, :edit, :show, :show] do
-      resources :meals do
-        resources :doses, only: [:create, :show, :new]
-      end
+  resources :contracts, only: [:new, :create] do
+    resources :programs, only: [:new, :create] do
     end
+  end
+
+  resources :programs, only: [] do
+    resources :meals, only: [:new, :create]
+  end
+
+  resources :programs, only: [] do
+    resources :sessions, only: [:new, :create]
+  end
+
+  resources :meals, only: [] do
+    resources :doses, only: [:new, :create]
   end
 
     resources :ingredients
