5b26924a54759bc054e317fd691bbd137dc4b9db
Hugo Bottois <bottois.hugo@outlook.fr>
2020-06-27 11:28:36 +0200

    coach bullet point in sign up

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,2 +1,27 @@
 class ProgramsController < ApplicationController
+
+  def new
+    @program = Program.new
+    @contract = Contract.find(params[:contract_id])
+  end
+
+  def create
+    @contract = Contract.find(params[:contract_id])
+    @program = Program.new(program_params)
+    @program.save
+  end
+
+  def show
+    @program = Program.find(params[:id])
+  end
+
+  def index
+    @programs = Program.all
+  end
+
+  private
+
+  def program_params
+    params.require(:program).permit(:proteins_target, :fats_target, :carbs_target, :contract_id)
+  end
 end
