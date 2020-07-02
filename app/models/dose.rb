fa2427118b4822a66e74cadd4471e657fadb1892
Hugo Bottois <bottois.hugo@outlook.fr>
2020-07-02 20:32:36 +0200

    validation model ingredient, meal doses

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,9 @@
 class Dose < ApplicationRecord
   belongs_to :meal
   belongs_to :ingredient
+
+
+  validates :quantity, presence: true
+  validates :meal, uniqueness: { scope: :ingredient }
+
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -3,4 +3,8 @@ class Meal < ApplicationRecord
 
   has_many :ingredients, through: :doses
   has_many :doses, dependent: :destroy
+
+
+  validates :name, presence: true
+  validates :start_time, presence: true
 end
