8555bdbafb4458c0342bb3a669588915acb26f8a
Hugo Bottois <bottois.hugo@outlook.fr>
2020-07-04 14:25:29 +0200

    fonctions helper in model meal

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -6,4 +6,16 @@ class Meal < ApplicationRecord
 
   validates :name, presence: true
   validates :start_time, presence: true
+
+  def proteins
+    self.doses.map { |dose| dose.quantity * dose.ingredient.proteins }.sum()/100
+  end
+
+  def fats
+    self.doses.map { |dose| dose.quantity * dose.ingredient.fats }.sum()/100
+  end
+
+  def carbs
+    self.doses.map { |dose| dose.quantity * dose.ingredient.carbs }.sum()/100
+  end
 end
