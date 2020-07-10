6655b6652f85b8cf8d911c6e10730dfe02be9be4
Hugo Bottois <bottois.hugo@outlook.fr>
2020-07-10 12:05:22 +0200

    refracto get calorie target from program

--- app/models/program.rb
+++ app/models/program.rb
@@ -3,9 +3,9 @@ class Program < ApplicationRecord
   has_many :meals
   has_many :sessions
 
+def calorie_target
+    self.proteins_target * 4 + self.fats_target * 9 + self.carbs_target * 4
 end
 
-
-
-
+end
 
