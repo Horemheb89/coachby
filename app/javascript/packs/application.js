9fdf71830afa31bfbd482439df70876b6e405932
Hadrien Cheru <hadrien.cheru@gmail.com>
2020-07-11 12:23:42 +0200

    Added Bootstrap tooltips

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -36,3 +36,7 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip();
+});
