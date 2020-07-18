034380b13a107ab985b0ca5a11917708a62687f4
Caroline Trenet <horemheb89@yahoo.co.uk>
2020-07-18 15:08:14 +0200

    active links sidebar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -36,7 +36,7 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { initProgressbarpro, initProgressbarfat, initProgressbarcarb, initProgressbarcalorie } from "../plugins/progressbar.js"
 import { initSelect2 } from "../plugins/init_select2"
-//= require select2_simple_form
+
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
