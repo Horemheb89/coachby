741b2d89b81f200284d4f2b1975ec976351e2169
quentin rouillon <quentinrouillon9@gmail.com>
2020-07-25 11:30:22 +0200

    html set up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,5 +17,8 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
-end
 
+  def default_url_options
+    { host: ENV["http://coachby-horemheb89.herokuapp.com/"] || "localhost:3000" }
+  end
+end
